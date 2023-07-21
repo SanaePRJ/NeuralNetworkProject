5346a9ac30408e6124707194d7168698c2c5af79
Sanae <usaginenetyann@gmail.com>
2023-07-21 17:14:05 +0900

    バグを修正しました。

--- NeuralNetworkProject/NeuralNetwork.h
+++ NeuralNetworkProject/NeuralNetwork.h
@@ -1,7 +1,7 @@
 /*=============================================================
 * NAME      : NeuralNetwork.h
 * AUTHOR    : SanaeProject
-* VER       : 1.0.1
+* VER       : 1.0.2
 * COPYRIGHGT: Copyright 2023 SanaeProject.
 =============================================================*/
 
@@ -21,32 +21,35 @@ namespace Sanae {
 	{
 	//Define Variable(public)
 	public:
-		double learn_late = 0.5;
+		double learn_late = 0.5;   //学習率
 		
 	
 	//Define Variable(private)
 	private:
-		Ulong In_Node     = 3;
-		Ulong Hidden_Node = 3;
-		Ulong Output_Node = 3;
+		Ulong In_Node     = 3;     //入力ノード数 
+		Ulong Hidden_Node = 3;     //隠れ層入力ノード数
+		Ulong Output_Node = 3;     //出力ノード数
 
-		Ulong  Node         = 3;
-		Ulong  Hidden_Layer = 3;
-		Ulong  IO_Layer     = 2;
+		Ulong  Hidden_Layer   = 3;   //隠れ層数
+		const Ulong  IO_Layer = 2;   //Input/Output用レイヤー
 
-		std::vector<Matrix> Weights;
+		std::vector<Matrix> Weights; //重み
 
 	
 	//Define Functions(private)
 	private:
+		//シグモイド関数
 		void Sigmoid
 		(
 			Matrix& _In
 		) 
 		{
+			//sigmoid(x)= 1 / (1+e^-x)
 			for (Ulong i = 0; i < _In.GetSize(); i++)
 				_In[i]   = 1 / (1 + std::exp(-1 * _In[i]));
 		}
+
+		//この関数により重みを改善する。
 		Matrix& Modifled
 		(
 			Matrix& In,
@@ -80,23 +83,30 @@ namespace Sanae {
 			: In_Node(_In_Node), Hidden_Node(_Hidden_Node), Output_Node(_Output_Node),
 			Hidden_Layer(_HLayer)
 		{
+			//乱数のシード値設定
 			srand(_Seed);
+			//各層の重みを設定
 			Weights.resize(Hidden_Layer+2);
 
+			//入力層->隠れ層
 			Weights[0]               .SetSize({In_Node    , Hidden_Node});
+			//隠れ層->出力層
 			Weights[Hidden_Layer + 1].SetSize({Hidden_Node, Output_Node});
 
+			//入力層重みの初期設定(0.00~0.99)
 			for (Ulong j = 0; j < Weights[0].GetSize(); j++)
-				(Weights[0])[j]                = (double)(rand() % 100) / 10;
+				(Weights[0])[j]                = (double)(rand() % 100) / 100;
 
+			//出力層重みの初期設定(0.00~0.99)
 			for (Ulong j = 0; j < Weights[Hidden_Layer + 1].GetSize(); j++)
-				(Weights[Hidden_Layer + 1])[j] = (double)(rand() % 100) / 10;
+				(Weights[Hidden_Layer + 1])[j] = (double)(rand() % 100) / 100;
 
+			//隠れ層重みの初期設定(0.00~0.99)
 			for (Ulong i = 1; i < Hidden_Layer+1; i++)
 			{
 				Weights[i].SetSize({Hidden_Node,Hidden_Node});
 				for (Ulong j = 0; j < Weights[i].GetSize(); j++)
-					(Weights[i])[j] = (double)(rand() % 100) / 10;
+					(Weights[i])[j] = (double)(rand() % 100) / 100;
 			}
 		}
 
@@ -134,13 +144,15 @@ namespace Sanae {
 			Matrix& Ideal
 		)
 		{
-			//各層の出力値を求める。
+			//各層出力格納用
 			std::vector<Matrix> Output;
 			Output.resize(this->Hidden_Layer+ IO_Layer);
 
+			//各層誤差格納用
 			std::vector<Matrix> Error;
 			Error.resize(this->Hidden_Layer + IO_Layer);
 
+			//出力を求める。
 			Output[0] = Weights[0] * In;
 			Sigmoid(Output[0]);
 
@@ -153,13 +165,13 @@ namespace Sanae {
 			Error[this->Hidden_Layer + 1] = Ideal - Output[this->Hidden_Layer + 1];
 			
 			for (Ulong i = this->Hidden_Layer; i > 0; i--)
-				Error[i] = Weights[i+1].Transpose() * Error[i + 1];
-			
+				Error[i] = Weights[i + 1].Transpose() * Error[i + 1];
+				
 			Error[0] = Weights[1].Transpose() * Error[1];
-			
+
 			//重みを調整する
 			Modifled(In, Weights[0], Error[0]);
-			for (Ulong i = 1; i < this->Hidden_Layer+ IO_Layer;i++)
+			for (Ulong i = 1; i < this->Hidden_Layer + IO_Layer; i++)
 				Modifled(Output[i - 1], Weights[i], Error[i]);
 		}
 	};
--- NeuralNetworkProject/NeuralNetworkProject.cpp
+++ NeuralNetworkProject/NeuralNetworkProject.cpp
@@ -12,15 +12,15 @@ void View(Matrix _D) {
 	std::cout << std::boolalpha;
 
 	for (Ulong i = 0; i < _D.GetSize(); i++)
-		std::cout << (_D[i] >= 0.5 ? true : false) << " ";
+		std::cout << (_D[i] > 0.5 ? true : false) << " ";
 }
 bool is_high(double In) {
 	return In >= 0.5;
 }
 
 void NotCircuit() {
-	NN Not         = { 1,2,1,1,(unsigned int)time(0UL)};
-	Not.learn_late = 0.3;
+	NN Not         = { 1,2,1,2,(unsigned int)time(0UL)};
+	Not.learn_late = 0.05;
 
 	int Accuracy = 0;
 	for (Ulong i = 0; i < 100; i++) {
@@ -52,41 +52,41 @@ void NotCircuit() {
 	printf("Not:正答率は%d%%です。\n",Accuracy);
 }
 void AndCircuit() {
-	NN And         = {2,3,1,2,(unsigned int)time(0UL)};
-	And.learn_late = 0.3;
+	NN And         = { 2,4,1,2,(unsigned int)time(0UL) };
+	And.learn_late = 0.07;
 
 	int Accuracy = 0;
 	for (Ulong i = 0; i < 100; i++) {
 		Matrix In = { {(double)is_high((double)(rand() % 100) / 100)},{(double)is_high((double)(rand() % 100) / 100)} };
 		Matrix buf = And.Out(In);
 
-		if (In[0] == In[1] == is_high(buf[0]))
+		if ((In[0] == In[1]) && In[0] == is_high(buf[0]))
 			Accuracy += 1;
 	}
 
 	printf("学習前And:正答率は%d%%です。\n", Accuracy);
 
 	for (Ulong i = 0; i < 50000; i++) {
-		Matrix In    = { {(double)is_high((double)(rand() % 100) / 100)},{(double)is_high((double)(rand() % 100) / 100)} };
-		Matrix Ident = { {(double)(In[0]==In[1])}};
-		
+		Matrix In =    { {(double)is_high((double)(rand() % 100) / 100)},{(double)is_high((double)(rand() % 100) / 100)} };
+		Matrix Ident = { {(double)((In[0] == In[1])&&In[0])}};
+			
 		And.Learn(In, Ident);
 	}
 
 	Accuracy = 0;
 	for (Ulong i = 0; i < 100; i++) {
-		Matrix In = { {(double)is_high((double)(rand() % 100) / 100)},{(double)is_high((double)(rand() % 100) / 100)} };
+		Matrix In  = { {(double)is_high((double)(rand() % 100) / 100)},{(double)is_high((double)(rand() % 100) / 100)} };
 		Matrix buf = And.Out(In);
 
-		if (In[0]==In[1]==is_high(buf[0]))
+		if (((In[0] == In[1]) && In[0])== is_high(buf[0]))
 			Accuracy += 1;
 	}
 
 	printf("And:正答率は%d%%です。\n", Accuracy);
 }
 void OrCircuit() {
-	NN Or         = {2,2,1,2,(unsigned int)time(0UL)};
-	Or.learn_late = 0.3;
+	NN Or         = {2,4,1,1,(unsigned int)time(0UL)};
+	Or.learn_late = 0.01;
 
 	int Accuracy = 0;
 	for (Ulong i = 0; i < 100; i++) {
